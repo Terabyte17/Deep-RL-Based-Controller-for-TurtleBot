5db43b17de4717e886c73f174b564e323e4935cc
Yash Sahijwani <60649571+Terabyte17@users.noreply.github.com>
2021-03-02 23:44:30 +0530

    PID control added

--- gym_test.py
+++ gym_test.py
@@ -4,8 +4,18 @@ import balance_bot
 import time
 
 if __name__=="__main__":
-    env = gym.make("balancebot-v0")
-    env.reset()
-    i = 0
+    env = gym.make("balancebot-PID-v0")
+    obs = env.reset()
+    error =  obs[0]
+    kp = 10
+    kd = 0.1
+    error_derivative = obs[1]
     while True:
-        env.step(4)
+        action = (kp*-error)/2 + kd*-error_derivative
+        print(action)
+        obs, _, done, _ = env.step(action)
+        error = obs[0]
+        error_derivative = obs[1]
+        if done:
+            obs = env.reset()
+
